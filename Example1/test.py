922e560a19c6d3ad222e49f8e53e1921238a1bc8
Gabriel Pallares <gabipallares@gmail.com>
2017-12-12 02:22:08 +0100

    test.py finished

--- Example1/test.py
+++ Example1/test.py
@@ -2,20 +2,26 @@ import sys
 print(sys.executable)
 print(sys.version)
 
+
 class Employee:
 
-	""" A sample Employee class"""
-	def __init__(self, first, last):
-		self.first = first
-		self.last = last
+    """ A sample Employee class"""
 
-	@property
-	def email(self):
-		return '{}.{}@email.com'.format(self.first, self.last)
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
 
-	@property
-	def fullname(self):
-		return '{}.{}'.format(self.first, self.last)
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{}.{}'.format(self.first, self.last)
+
+    for num in [1, 2, 3, 4]:
+        print(num)
+
 
 emp_1 = Employee('John', 'Smith')
 
